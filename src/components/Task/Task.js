6d5539681a335c83ca66c70bcf29e3f152093b82
Daria <d.a.kireeva@mail.ru>
2023-10-18 21:43:06 +0300

    add styles for cursor

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -3,7 +3,11 @@ import "./Task.scss";
 
 const Task = ({ task, ...props }) => {
   const ActionBtn = () => <div className="task__done">{task.done ? <p onClick={props.handleTask}>✅</p> : <p onClick={props.handleTask}>✘</p>}</div>;
-  const DeleteBtn = () => <div onClick={props.deleteTask}>Удалить задачу</div>;
+  const DeleteBtn = () => (
+    <div className="task__delete" onClick={props.deleteTask}>
+      Удалить задачу
+    </div>
+  );
 
   const className = "task" + ` ${task.done ? "done" : ""}`;
 
